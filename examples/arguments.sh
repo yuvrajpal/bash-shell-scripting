9201c60f6af9b733908571c8817095cdd07761df
Yuvraj Pal <yuvraj.pal.contractor@move.com>
2022-01-13 16:45:02 +0530

    argument and linecount

---
+++ examples/arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# example of using arguments to a script
+echo "My first name is $1"
+echo "My surname is $2"
+echo "Total number of arguments is $#" 
+
+
+
+# How to run?
+# ./name.sh Hans-Wolfgang Loidl
